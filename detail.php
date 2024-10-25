5d2723e02a8453ee417a8c05a19316008ab6f74d
Leonardo-Scotti <scottileonardo1@gmail.com>
2024-10-25 17:37:01 -0300

    Criando Array

---
+++ detail.php
@@ -0,0 +1,5 @@
+<?php
+require 'includes/header.php';
+require 'includes/produto.php';
+require 'includes/home.php';
+require 'includes/footer.php';
---
+++ includes/produtos.php
@@ -0,0 +1,150 @@
+<?php
+$produtos = [
+    [
+        'nome' => ' Tênis Nike Air Max Flyknit Racer Feminino',
+        'modelo' => 'air-max-flyknit-racer-',
+        'categoria' => 'Corrida',
+        'preco' => '1.315.99',
+        'desconto' => '6',
+        'cores' => ['cinza'],
+        'tamanhos' => [35, 36, 37, 38, 39, 40, 41, 42, 43, 44]
+
+    ],
+    [
+        'nome' => 'Tênis Nike Air Max SYSTM Feminino',
+        'modelo' => 'air-max-systm-',
+        'categoria' => 'Casual',
+        'preco' => '434.99',
+        'desconto' => '42',
+        'cores' => ['bege', 'preto'],
+        'tamanhos' => [35, 36, 37, 38, 39, 40, 41, 42, 43, 44]
+
+    ],
+    [
+        'nome' => 'Tênis Nike Dunk Low Feminino Blue Paisley',
+        'modelo' => 'dunk-low',
+        'categoria' => 'Casual',
+        'preco' => '899.99',
+        'desconto' => 25,
+        'cores' => ['blue-paisley'],
+        'tamanhos' => [35, 36, 37, 38, 39, 40, 41, 42, 43, 44]
+    ],
+    [
+        'nome' => 'Tênis Jordan Series Feminino',
+        'modelo' => 'jordan-series',
+        'categoria' => 'Casual',
+        'preco' => '759.99',
+        'desconto' => '5',
+        'cores' => ['verde'],
+        'tamanhos' => [35, 36, 37, 38, 39, 40, 41, 42, 43, 44]
+        
+    ],
+    [
+        'nome' => 'Tênis Nike Juniper Trai 2 GORE-TEX Masculino',
+        'modelo' => 'juniper-trail-2-gore-tex',
+        'categoria' => 'Corrida',
+        'preco' => '1.215.99',
+        'desconto' => '24',
+        'cores' => ['branco', 'cinza'],
+        'tamanhos'=> [35, 36, 37, 38, 39, 40, 41, 42, 43, 44]
+    ],
+    [
+        'nome' => 'Tênis Nike Metcon 8 Masculino',
+        'modelo' => 'metcon-9',
+        'categoria' => 'Treino & Academia',
+        'preco' => '983.99',
+        'desconto' => '18',
+        'cores' => ['preto', 'vermelho'],
+        'tamanhos'=> [35, 36, 37, 38, 39, 40, 41, 42, 43, 44]
+    ],
+    [
+        'nome' => 'Tênis nike Pegasus 40 Premium Feminino',
+        'modelo' => 'pegasus-40-premium',
+        'categoria' => 'Corrida',
+        'preco' => '681.99',
+        'desconto' => '38',
+        'cores' => ['rosa'],
+        'tamanhos'=> [35, 36, 37, 38, 39, 40, 41, 42, 43, 44]
+    ],
+    [
+        'nome' => 'Chuteira Nike Phantom GT2 Elite SG Pro Campo',
+        'modelo' => 'phantom-gt2-elite',
+        'categoria' =>'Campo',
+        'preco' => '1.899.99',
+        'desconto' => '5',
+        'cores' => ['azul', 'cinza', 'preto'],
+        'tamanhos'=> [35, 36, 37, 38, 39, 40, 41, 42, 43, 44]
+    ],
+    [
+        'nome' => 'Tênis Nike React Infinity Run FK 3 Feminino',
+        'modelo' => 'react-infinity-run-fk-3',
+        'categoria' => 'Corrida',
+        'preco' => '949.99',
+        'desconto' => '5',
+        'cores' => ['rosa'],
+        'tamanhos'=> [35, 36, 37, 38, 39, 40, 41, 42, 43, 44]
+    ],
+    [
+        'nome' => 'Tênis Nike Renew In-Season TR 12 Feminino',
+        'modelo' => 'renew-in-season-tr-12',
+        'categoria' => 'Treino & Academia',
+        'preco' => '617.49',
+        'desconto' => '5',
+        'cores' => ['Cinza', 'preto'],
+        'tamanhos'=> [35, 36, 37, 38, 39, 40, 41, 42, 43, 44]
+    ],
+    [
+        'nome' => 'Tênis Nike Terminator High',
+        'modelo' => 'terminator-high',
+        'categoria' => 'Casual',
+        'preco' => '659.99',
+        'desconto' => '34',
+        'cores' => ['bege', 'cinza'],
+        'tamanhos'=> [35, 36, 37, 38, 39, 40, 41, 42, 43, 44]
+    ],
+    [
+        'nome' => 'Chuteira Nike Tiempo Legend 9 Elite Campo',
+        'modelo' => 'tiempo-legend-9',
+        'categoria' => 'Campo',
+        'preco' => '1.419.99',
+        'desconto' => '29',
+        'cores' => ['branco', 'preto'],
+        'tamanhos'=> [35, 36, 37, 38, 39, 40, 41, 42, 43, 44]
+    ],
+    [
+        'nome' => 'Tênis Nike Zoom Fly 5 Feminino',
+        'modelo' => 'zoom-fly-5',
+        'categoria' => 'Corrida',
+        'preco' => '1.000.99',
+        'desconto' => '23',
+        'cores' => ['branco', 'roxo'],
+        'tamanhos'=> [35, 36, 37, 38, 39, 40, 41, 42, 43, 44]
+    ],
+    [
+        'nome' => 'Tênis Nike Zoom Rotational 6 Unissex',
+        'modelo' => 'zoom-rotational-6',
+        'categoria' => 'Corrida',
+        'preco' => '620.49',
+        'desconto' => '27',
+        'cores' => ['preto', 'azul'],
+        'tamanhos'=> [35, 36, 37, 38, 39, 40, 41, 42, 43, 44]
+    ],
+    [
+        'nome' => 'Chuteira Nike Zoom Superfly 9 Elite KM Campo',
+        'modelo' => 'zoom-superfly-9',
+        'categoria' => 'Campo',
+        'preco' => '1.624.99',
+        'desconto' => '35',
+        'cores' => ['azul'],
+        'tamanhos'=> [35, 36, 37, 38, 39, 40, 41, 42, 43, 44]
+    ],
+    [
+        'nome' => 'Tênis Nike Zoom Vapor Cage 4 Rafa Masculino',
+        'modelo' => 'zoom-vapor-cage-4-rafa',
+        'categoria' => 'Jogar Tênis',
+        'preco' => '1.169.99',
+        'desconto' => '22',
+        'cores' => ['amarelo', 'branco'],
+        'tamanhos'=> [35, 36, 37, 38, 39, 40, 41, 42, 43, 44]
+    ]
+    ];
